acaea4b37450a85b456dcafc5ae2de505a0b8d37
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2022-10-17 16:41:47 -0400

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,23 +1,23 @@
 
 $(function(){
     $("#Areas_of_research_1").on("click",function(){
-        window.location = "index-Areas_of_research.html#Areas_research_11";
+        window.location = "areas_of_research.html#Areas_research_11";
     });
 });
 
 $(function(){
     $("#Areas_of_research_2").on("click",function(){
-        window.location = "index-Areas_of_research.html#Areas_research_22";
+        window.location = "areas_of_research.html#Areas_research_22";
     });
 });
 $(function(){
     $("#Areas_of_research_3").on("click",function(){
-        window.location = "index-Areas_of_research.html#Areas_research_33";
+        window.location = "areas_of_research.html#Areas_research_33";
     });
 });
 $(function(){
     $("#Areas_of_research_4").on("click",function(){
-        window.location = "index-Areas_of_research.html#Areas_research_44";
+        window.location = "areas_of_research.html#Areas_research_44";
     });
 });
 
@@ -27,43 +27,43 @@ $(function(){
 /////////////////////////////////////////////////
 $(function(){
     $("#home_1").on("click",function(){
-        window.location = "index-main.html#home_11";
+        window.location = "index.html#home_11";
     });
 });
 $(function(){
     $("#home_2").on("click",function(){
-        window.location = "index-main.html#home_22";
+        window.location = "index.html#home_22";
     });
 });
 $(function(){
     $("#home_3").on("click",function(){
-        window.location = "index-main.html#home_33";
+        window.location = "index.html#home_33";
     });
 });
 $(function(){
     $("#home_4").on("click",function(){
-        window.location = "index-main.html#home_44";
+        window.location = "index.html#home_44";
     });
 });
 ////////////////////////////////////////////////////////////////
 $(function(){
     $("#empowerment_1").on("click",function(){
-        window.location = "index.empowerment.html#empowerment_11";
+        window.location = "empowerment.html#empowerment_11";
     });
 });
 
 $(function(){
     $("#empowerment_2").on("click",function(){
-        window.location = "index.empowerment.html#empowerment_22";
+        window.location = "empowerment.html#empowerment_22";
     });
 });
 $(function(){
     $("#empowerment_3").on("click",function(){
-        window.location = "index.empowerment.html#empowerment_33";
+        window.location = "empowerment.html#empowerment_33";
     });
 });
 $(function(){
     $("#empowerment_4").on("click",function(){
-        window.location = "index.empowerment.html#empowerment_44";
+        window.location = "empowerment.html#empowerment_44";
     });
 });
