f61b0e2a96d6a0c46b6d38507c41aafaa5b64d0e
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2022-10-21 16:55:33 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,19 +8,19 @@ $("#home_1").click(function () {
 $("#home_2").click(function () {
     //animate html body and use jQuery scrollTop
     $('html, body').animate({
-        scrollTop: $("#home_22").offset().top -20
+        scrollTop: $("#home_22").offset().top -30
     }, 1000);
 });
 $("#home_3").click(function () {
     //animate html body and use jQuery scrollTop
     $('html, body').animate({
-        scrollTop: $("#home_33").offset().top -20
+        scrollTop: $("#home_33").offset().top -30
     }, 1000);
 });
 $("#home_4").click(function () {
     //animate html body and use jQuery scrollTop
     $('html, body').animate({
-        scrollTop: $("#home_44").offset().top -20
+        scrollTop: $("#home_44").offset().top -30
     }, 1000);
 });
 
