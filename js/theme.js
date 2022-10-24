8506cbe4390cbfa6dea9bf2a3c2926f479352093
RavinduTharanga <113649586+RavinduTharanga@users.noreply.github.com>
2022-10-24 13:08:22 -0400

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -364,7 +364,7 @@
 $(window).on ('load', function (){ // makes sure the whole site is loaded
 
 // -------------------- Site Preloader
-        $('#ctn-preloader').fadeOut(); // will first fade out the loading animation
+        $('#ctn-preloade').fadeOut(); // will first fade out the loading animation
         $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
         $('body').delay(350).css({'overflow':'visible'});
 
